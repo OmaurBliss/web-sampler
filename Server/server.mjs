e6947ff40f8f5de304404d7b38488d278f365c47
Omaur Bliss <omaur.bliss@concordusa.com>
2023-11-02 13:38:03 -0500

    added back end

---
+++ Server/server.mjs
@@ -0,0 +1,17 @@
+import express from "express";
+import cors from "cors";
+import "./loadEnvironment.mjs";
+import records from "./Routes/record.mjs";
+
+const PORT = 5000;
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/record", records);
+
+// start the Express server
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import React from "react";
 import reactDom from "react-dom";
-import App from "./public/src/App"
+import App from "./public/src/App";
 
 reactDom.render(<App />, document.getElementById("root"));
